6ebd2e0f3a69cbe91aa0efb96e341e72c293b1ce
James Bonura <jbbonura@gmail.com>
2022-05-05 14:27:51 -0700

    Added button with incrementing state: age

--- prop_it_up/src/components/PersonCard.jsx
+++ prop_it_up/src/components/PersonCard.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 
 class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    incrementAge = () => {
+        let {age} = this.state;
+        this.setState({age: age+1});
+    } 
     render() {
-        const {firstName, lastName, age, hairColor:color} = this.props;
+        const {firstName, lastName, hairColor:color} = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {color}</p>
+                <button onClick = {this.incrementAge}>Birthday Button for {firstName} {lastName}</button>  
             </div>
         )
     }
