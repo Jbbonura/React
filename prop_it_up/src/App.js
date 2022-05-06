7d354a93629153f9ffcbeb25cc1834b459e440d2
James Bonura <jbbonura@gmail.com>
2022-05-06 12:17:06 -0700

    changed to functional components

--- prop_it_up/src/App.js
+++ prop_it_up/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import React, {useState} from 'react';
+import PersonCard from './components/PersonCard';
 
-import PersonCard from './components/PersonCard'
 function App() {
+  //set information
+  const [users, setUsers] = useState([
+    {firstName: "Jane", lastName:"Doe", age:45, hairColor:"Black"},
+    {firstName: "John", lastName:"Smith", age:88, hairColor:"Brown"},
+    {firstName: "Millard", lastName:"Fillmore", age:50, hairColor:"Brown"},
+    {firstName: "Maria", lastName:"Smith", age:62, hairColor:"Brown"}
+  ]);
+
+  
+ 
   return (
     <div>
-      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
-      <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
-      <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
-      <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
+      {
+        users.map((person, index) => <PersonCard key={index} user={person}/>)
+      }
     </div>
   );
 }
--- prop_it_up/src/components/PersonCard.jsx
+++ prop_it_up/src/components/PersonCard.jsx
@@ -1,27 +1,23 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class PersonCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age
-        }
-    }
-    incrementAge = () => {
-        let {age} = this.state;
-        this.setState({age: age+1});
-    } 
-    render() {
-        const {firstName, lastName, hairColor:color} = this.props;
-        return (
+const PersonCard = (props) => {
+    //create new state
+    const [age, setAge] = useState(props.user.age);
+    //create function
+    const incrementAge = () => {
+        setAge(age+1);
+      }
+    //Destructure the props information
+    const{firstName, lastName, hairColor} = props.user;
+    return(
+        
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {color}</p>
-                <button onClick = {this.incrementAge}>Birthday Button for {firstName} {lastName}</button>  
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick = { () => incrementAge()}>Birthday Button for {firstName} {lastName}</button>  
             </div>
-        )
-    }
+    );
 }
 
 export default PersonCard;
